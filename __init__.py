b2bf1e780b9275b8ea4ddf813ed60c601220d153
thomasm0077 <thomasm0077@gmail.com>
2024-07-05 00:22:45 +0100

    Added main Init

---
+++ __init__.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('app.config.Config')
+
+    db.init_app(app)
+
+    return app
