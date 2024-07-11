ebdfcdaccc16145ff2fe94607e74c3212c5d35f2
thomasm0077 <thomasm0077@gmail.com>
2024-07-11 22:34:08 +0100

    bug fix - static files not found

--- app/login/__init__.py
+++ app/login/__init__.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
 
-bp = Blueprint('login', __name__, template_folder="templates")
+bp = Blueprint('login', __name__, static_folder='static', template_folder="templates")
 
 from app.login import routs
