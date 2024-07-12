24117fabcfb436cc3e598245763069831542600c
Liya Cheriyachan <20034649@mydbs.ie>
2024-07-12 15:57:47 +0100

    bug fixed login, added router for register

--- app/login/routs.py
+++ app/login/routs.py
@@ -1,6 +1,6 @@
 from flask import request, render_template, redirect, url_for, flash
 from app.login import bp
-from app.login.models import User as logindb
+from app.models import User as logindb
 
 @bp.route('/')
 def index():
@@ -12,13 +12,43 @@ def login():
         user_name = request.form['username']
         password = request.form['password']
         singleUser = logindb.query.filter_by(username=user_name).first()
-        print(singleUser.username)
+        print(singleUser)
+        if singleUser == None:
+            flash("No username assosiated found!!", "danger")
+        else:          
+            if user_name == "" or password == "":
+                flash("Please fill in all fields.")
+            elif password == singleUser.password:
+                return redirect(url_for('home'))
+            else:
+                flash("Invalid username or password.", "danger")
 
-        if user_name == "" or password == "":
-            flash("Please fill in all fields.")
-        elif password == singleUser.password:
-            return redirect(url_for('home'))
-        else:
-            flash("Invalid username or password.", "danger")
+    return render_template('login.html')
+
+@bp.route('/register')
+def go_to_register():
+    return render_template('register.html')
+
+@bp.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        user_name = request.form['username']
+        password = request.form['password']
+        conf_password = request.form['confirm_password']
+        if password == conf_password:
+            record = logindb()
+
+
+        singleUser = logindb.query.filter_by(username=user_name).first()
+        print(singleUser)
+        if singleUser == None:
+            flash("No username assosiated found!!", "danger")
+        else:          
+            if user_name == "" or password == "":
+                flash("Please fill in all fields.")
+            elif password == singleUser.password:
+                return redirect(url_for('home'))
+            else:
+                flash("Invalid username or password.", "danger")
 
     return render_template('login.html')
