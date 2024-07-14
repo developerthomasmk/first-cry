9c99e63985155894dc6064cf766f625a2ae142e8
Nihal Faiz <nihalfaiz@Nihals-MacBook-Pro.local>
2024-07-14 17:23:29 +0100

    Updating home page

--- app/home/routes.py
+++ app/home/routes.py
@@ -1,6 +1,57 @@
-from flask import request, redirect, url_for, render_template
+from flask import request, redirect, session, url_for, render_template, jsonify, flash
 from app.home import bp
+from app.models import Products as productdb
+from app.models import User as logindb
 
 @bp.route('/')
 def index():
     return render_template('home.html')
+
+@bp.route('/has_session')
+def hasSession():
+    if 'user_id' in session:
+        user = logindb.query.filter_by(id=session['user_id']).first()
+        return jsonify({'has_data': True, 'username': user.firstname})
+    else:
+        return jsonify({'has_data': False})
+
+@bp.route('/get_products')
+def getProducts():
+    if 'user_id' in session:
+        print(session['user_id'])
+        userId = session['user_id']
+        products = productdb.query.filter(productdb.user_id != userId, productdb.status == 'active').all()
+        if len(products) > 0:
+            products_dict = [product.to_dict() for product in products]
+            response = jsonify({'status':'success','msg': 'success!!', 'data': products_dict})
+            print(response)
+            return response
+        else:
+            return jsonify({'status':'failed', 'msg': 'No data found!'})
+        
+    else:
+        return jsonify({'status':'failed', 'msg': 'user not found'})
+    
+    
+@bp.route('/get_products')
+def getAdminProducts():
+    products = productdb.query.filter(productdb.status != 'active').all()
+    if len(products) > 0:
+        products_dict = [product.to_dict() for product in products]
+        response = jsonify({'status':'success','msg': 'success!!', 'data': products_dict})
+        print(response)
+        return response
+    else:
+        return jsonify({'status':'failed', 'msg': 'No data found!'})
+    
+@bp.route('/login')
+def go_to_login():
+    return redirect(url_for('login.login'))
+
+@bp.route('/history')
+def go_to_history():
+    return redirect(url_for('history.index'))
+
+@bp.route('/sell_product')
+def go_to_sell():
+    return redirect(url_for('sell_product.addProduct'))
---
+++ app/home/static/js/script.js
@@ -0,0 +1,85 @@
+document.addEventListener('DOMContentLoaded', () => {
+    checkSession()
+    fetchUserData();
+});
+
+async function fetchUserData() {
+    try {
+        const response = await fetch(getDataUrl);
+        const data = await response.json();
+        console.log(data)
+        if (data.status != 'failed') {
+            loadData(data)
+        }
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+
+function loadData(dataJson) {
+    var homeContainer = document.getElementById("home_container");
+    homeContainer.innerHTML = "";
+
+    console.log(dataJson.data)
+
+    dataJson.data.forEach((data, index) => {
+
+        const productUrl = `${productImageUrl}/${data.image}`
+
+        var eqpEl = `<div class="col-sm-6 col-md-4 col-lg-3">
+          <div class="box">
+            <a href="">
+              <div class="img-box">
+                <img src="${productUrl}" alt="">
+              </div>
+              <div class="detail-box">
+                <h6>
+                  robot 
+                </h6>
+                <h6>
+                  ${data.productname}
+                  <span>
+                    &#8364;${data.price}
+                  </span>
+                </h6>
+              </div>
+            </a>
+          </div>
+        </div>`;
+
+        homeContainer.innerHTML += eqpEl;
+    });
+}
+
+async function checkSession() {
+    try {
+        const response = await fetch(hasSessionUrl);
+        const data = await response.json();
+        var loginContainer = document.getElementById("login_container");
+        loginContainer.innerHTML = "";
+
+        if (data.has_data) {
+
+
+            var eqpEl = `<a href="">
+              <i class="fa fa-user" aria-hidden="true"></i>
+              <span>
+                Welcome ${data.username}
+              </span>
+            </a>`;
+
+
+
+        } else {
+            var eqpEl = `<a href=${getLoginUrl}>
+              <i class="fa fa-user" aria-hidden="true"></i>
+              <span>
+                LOGIN
+              </span>
+            </a>`;
+        }
+        loginContainer.innerHTML += eqpEl;
+    } catch (error) {
+        console.error('Error checking session:', error);
+    }
+}
